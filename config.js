0a6729fed326af2abff1266b115873a0dcc0160b
me4127-2026 <me4127.26@gmail.com>
2026-07-21 06:44:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const BACKEND_URL = "https://script.google.com/macros/s/AKfycbx-Jwj_rfvrS6cp4xU4Euqgdlu-p6GgqMfGH26ZpR0iNzTYiZ8euMDWoIWJMfCRhejJaQ/exec"
+const BACKEND_URL = "https://script.google.com/macros/s/AKfycbybv9zCmPV0Pcn_B73k7N3EzkTpyVtfdTSNbhfbbzdbiadZZQmuPz4TCTSlH0op27Y6tg/exec"
